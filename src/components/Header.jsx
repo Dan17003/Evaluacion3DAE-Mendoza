565fdd86afb9d2ac7b8d6080419a8370ff304fd2
Dan17003 <danna.mendoza@tecsup.edu.pe>
2025-06-04 15:20:56 -0500

    cambios finales

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,13 +5,13 @@ export default function Header() {
   return (
     <Navbar bg="dark" variant="dark" expand="lg">
       <Container>
-        <Navbar.Brand as={Link} to="/">BCP Online</Navbar.Brand>
+        <Navbar.Brand as={Link} to="/">Movie App Online</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse>
           <Nav>
             <Nav.Link as={Link} to="/">Inicio</Nav.Link>
-            <Nav.Link as={Link} to="/items">Productos</Nav.Link>
-            <Nav.Link as={Link} to="/contact">Contacto</Nav.Link>
+            <Nav.Link as={Link} to="/items">Peliculas</Nav.Link>
+            
           </Nav>
         </Navbar.Collapse>
       </Container>
